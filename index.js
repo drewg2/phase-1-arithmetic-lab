9ef0bf82036bce7e9c5b8c3cd542e4a96f8d53c8
Drew Grey <dtsgrey@gmail.com>
2022-02-01 09:44:45 -0600

    commit tree 1f0cb195bf3213f7333c01b0685475c00b6c169e

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+const num1 = 31;
+const num2 = 2;
+let multiply = num1 * num2;
 
-// Write your code here
+let random = Math.floor(Math.random() * 10) + 1;
+
+const num3 = 19;
+const num4 = 5;
+let mod = Math.round(num3 / num4);
+
+let max = Math.max(2,4,6,8,10,12,14,16,18,20);
+
+console.log( 9000 < 9001);
